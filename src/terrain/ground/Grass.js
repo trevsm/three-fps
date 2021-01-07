26dda1a0637cc1c03e90647e0b2cf0b3923925f8
trevsm <trevsm@pm.me>
2021-01-07 16:29:13 -0700

    changed grass slightly, cross orientation

--- src/terrain/ground/Grass.js
+++ src/terrain/ground/Grass.js
@@ -1,16 +1,17 @@
 import React, { useRef, useLayoutEffect, useMemo, Suspense } from 'react'
 import { perlin } from '../functions/Math'
 import * as THREE from 'three'
-import grass from './textures/grass_blades.png'
-import { useLoader } from 'react-three-fiber'
+import { useFrame, useLoader } from 'react-three-fiber'
 // import { useFrame } from 'react-three-fiber'
 
-export function Grass(props) {
+export function Plants(props) {
   const ref = useRef()
+  const ref2 = useRef()
+  const S = 2
 
-  const count = Math.pow(300, 2)
+  const count = Math.pow(props.count, 2)
   const cX = Math.pow(count, 1 / 2) / 2
-  const sX = 1 // spacing
+  const sX = props.spread // spacing
 
   const { positions, transform } = useMemo(() => {
     const positions = [...Array(count)].map(() => new THREE.Vector3())
@@ -23,58 +24,61 @@ export function Grass(props) {
     let y = 0
     for (let x = -cX; x < cX; x++) {
       for (let z = -cX; z < cX; z++) {
-        y = 1 * perlin.get(0.2 * x, 0.2 * z)
+        y = props.amp * perlin.get(0.1 * x, 0.1 * z)
         positions[index].set(
           x * sX + Math.random() * sX,
           y,
           z * sX + Math.random() * sX
         )
         transform.setPosition(positions[index])
+
         ref.current.setMatrixAt(index, transform)
+        ref2.current.setMatrixAt(index, transform)
+
         index++
       }
     }
   }, [])
 
-  const grassBaseGeometry = new THREE.PlaneBufferGeometry(2, 2)
+  const grassBaseGeometry = new THREE.PlaneBufferGeometry(S, S)
+  const grassBaseGeometry2 = new THREE.PlaneBufferGeometry(S, S)
+
   const positionAttribute = grassBaseGeometry.attributes.position
+  const positionAttribute2 = grassBaseGeometry2.attributes.position
 
   for (let i = 0; i < positionAttribute.count; i++) {
     let x = positionAttribute.getX(i)
     let y = positionAttribute.getY(i)
     let z = positionAttribute.getZ(i)
 
-    // z += Math.random() * 0.1
-
     positionAttribute.setXYZ(i, x, y, z)
+    positionAttribute2.setXYZ(i, z, y, x)
   }
 
-  const texture = useLoader(THREE.TextureLoader, grass)
+  const texture = useLoader(THREE.TextureLoader, props.texture)
 
   return (
-    <group {...props}>
-      <instancedMesh args={[grassBaseGeometry, null, count]} ref={ref}>
-        <meshLambertMaterial
-          side={THREE.DoubleSide}
-          alphaTest={0.5}
-          transparent
-          opacity={1}
-          map={texture}
-        />
-      </instancedMesh>
-    </group>
-  )
-}
-
-export function GrassPatch(props) {
-  return (
-    <Suspense fallback={null}>
-      <group {...props}>
-        <Grass rotation={[0, (Math.PI * 1) / 4, 0]} />
-        <Grass rotation={[0, (Math.PI * 2) / 4, 0]} />
-        <Grass rotation={[0, (Math.PI * 3) / 4, 0]} />
-        <Grass rotation={[0, (Math.PI * 4) / 4, 0]} />
+    <group rotation={props.rotation}>
+      <group>
+        <instancedMesh args={[grassBaseGeometry, null, count]} ref={ref}>
+          <meshStandardMaterial
+            side={THREE.DoubleSide}
+            alphaTest={0.5}
+            transparent
+            map={texture}
+          />
+        </instancedMesh>
       </group>
-    </Suspense>
+      <group>
+        <instancedMesh args={[grassBaseGeometry2, null, count]} ref={ref2}>
+          <meshStandardMaterial
+            side={THREE.DoubleSide}
+            alphaTest={0.5}
+            transparent
+            map={texture}
+          />
+        </instancedMesh>
+      </group>
+    </group>
   )
 }
--- src/terrain/ground/Ground.js
+++ src/terrain/ground/Ground.js
@@ -1,10 +1,28 @@
 import * as THREE from 'three'
-import React from 'react'
+import React, {Suspense} from 'react'
 
 import { useLoader } from 'react-three-fiber'
 import { usePlane } from 'use-cannon'
-import Dirt from './textures/dirt.png'
-import { GrassPatch } from './Grass'
+import dirt from './textures/dirt.png'
+import grass from './textures/grass_blades.png'
+import { Plants } from './Grass'
+
+function Foliage(props) {
+  return (
+    <Suspense fallback={null}>
+      <group position={props.position}>
+        <Plants
+          rotation={props.rotation}
+          texture={props.texture}
+          count={props.count}
+          spread={props.spread}
+          amp={props.amp}
+          width={props.width}
+        />
+      </group>
+    </Suspense>
+  )
+}
 
 export default props => {
   const mapWidth = 1000,
@@ -17,13 +35,19 @@ export default props => {
     args: [mapWidth, mapLength, vX, vX],
   }))
 
-  const texture = useLoader(THREE.TextureLoader, Dirt)
+  const texture = useLoader(THREE.TextureLoader, dirt)
   texture.wrapS = texture.wrapT = THREE.RepeatWrapping
   texture.repeat.set(mapWidth / 4, mapLength / 4)
 
   return (
-    <>
-      <GrassPatch position={[0, 1, 0]} />
+    <group>
+      <Foliage
+        texture={grass}
+        count={300}
+        spread={1}
+        amp={.5}
+        position={[0, 1, 0]}
+      />
       <mesh ref={ref} receiveShadow>
         <planeBufferGeometry
           attach="geometry"
@@ -31,6 +55,6 @@ export default props => {
         />
         <meshStandardMaterial attach="material" map={texture} />
       </mesh>
-    </>
+    </group>
   )
 }
